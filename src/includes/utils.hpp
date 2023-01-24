fc4c74517ffc6962fb73d6dc7d35321bb752aa71
JARDOT CHARLES <charles.jardot@etu.unistra.fr>
2023-01-24 11:58:10 +0100

    max

--- src/includes/utils.hpp
+++ src/includes/utils.hpp
@@ -5,7 +5,15 @@
 void incremente(int& x);
 
 /* Affichage sur la sortie standard */
-/* Penser à préciser le type de variable avec printobj<int> par ex */
+/* Penser à préciser le type de variable avec printObj<int> par ex */
 template <typename T>
 void printObj(const T& obj);
+
+/* Fonction de cast vers int */
+template <typename T>
+int to_int(T value);
+
+/* Retourne le max entre deux valeurs avec comme type T1 pour la val retournée */
+template <typename T1, typename T2> 
+T1 max(T1 a, T2 b);
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -13,3 +13,15 @@ void printObj(const T& obj){
     cout << obj << std::endl;
 }
 
+/* Fonction de cast vers int */
+template <typename T>
+int to_int(T value) {
+    return static_cast<int>(value);
+}
+
+/* Retourne le max entre deux valeurs avec comme type T1 pour la val retournée */
+template <typename T1, typename T2>
+T1 max(T1 a, T2 b) {
+    return (a > b) ? a : b;
+}
+
