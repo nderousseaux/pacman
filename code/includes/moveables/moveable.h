735d4351462b1b2b6e2b6f9e4a527a99f3f97df5
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2023-05-07 19:50:09 +0200

    MOVABLE

--- code/includes/moveables/moveable.h
+++ code/includes/moveables/moveable.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "element.h"
-#include "intersection.h"
 #include "direction.h"
+#include "intersection.h"
 
 #include <SDL2/SDL.h>
 
@@ -14,10 +14,10 @@ class Moveable: public Element {
 
     /* Getter/setters */
     void set_direction(Direction direction) {_direction = direction;}
+    Direction get_direction(){return _direction;}
 
     /* Méthodes */
     virtual void spawn();   // Fait réapparaître l'élément
-    Direction get_dir(){return _direction;}
 
   protected:
     /* Variables d'instance */
@@ -25,12 +25,10 @@ class Moveable: public Element {
     Direction _direction = STOP;           // Direction de déplacement de l'élément
     Direction _next_direction = STOP;      // Direction prise à l'arrivée de la prochaine intersection
 
-    /* Getters/Setters commun à tout les moveables */
+    /* Getters/Setters communs à tout les Moveables */
     virtual int get_speed() = 0;
     virtual Intersection * get_start() = 0;
     Intersection * get_destination(){return _destination;};
-
-    /* Getters/Setters */
     void set_x(int x);
     void set_y(int y);
 
